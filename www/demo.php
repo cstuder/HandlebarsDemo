6120b130fe558dddae216bfec7426550b99dd91b
Christian Studer <cstuder@existenz.ch>
2017-12-29 13:54:50 +0100

    Working PHP basic demo.

--- www/demo.php
+++ www/demo.php
@@ -5,5 +5,28 @@
  * @author Christian Studer <cstuder@existenz.ch>
  */
 
- // TODO do this
- 
+// Composer autoload
+require_once('../vendor/autoload.php');
+
+// Get data
+$data = json_decode(file_get_contents('https://aareguru.existenz.ch/currentV2.php'));
+
+// Get template
+$template = file_get_contents('template.txt');
+
+// Render with handlebars
+$engine = new Handlebars\Handlebars;
+$content = $engine->render($template, $data);
+
+// Output HTML
+?>
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <title>Aare.guru - Handlebars Demo</title>
+    </head>
+    <body>
+        <?php echo $content; ?>
+    </body>
+</html>
